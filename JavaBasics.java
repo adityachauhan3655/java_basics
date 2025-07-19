e63ff2bb15d793e6ca99be4d71ee8000f636a4f1
Aditya Chauhan <adityachauhan3655@gmail.com>
2025-07-19 09:46:48 +0530

    Adding the funtion of hollow rectangle

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,22 +1,40 @@
 import java.util.*;
 
 public class JavaBasics {
+    public static void hollow_rec(int totrow, int totcol) {
+        // Outerloop for row
+        for (int i = 1; i <= totrow; i++) {
+            // Inner loop for column
+            // cell(i,j)
+            for (int j = 1; j <= totcol; j++) {
+                if (i == 1 || i == totrow || j == 1 || j == totcol) {
+                    //boundary cell
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void half_inverted_pyramid(int n) {
         //outerloop for rows
-        for(int i = 1; i<=n; i++){
+        for (int i = 1; i <= n; i++) {
             //spaces
-            for(int j=1; j<=n-i; j++){
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
             // stars
-            for(int j=1; j<=i; j++){
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 
-   public static void main (String[]args){
-        half_inverted_pyramid(5);
-   }
+    public static void main(String[] args) {
+//        half_inverted_pyramid(5);
+        hollow_rec(4,6);
+    }
 }
